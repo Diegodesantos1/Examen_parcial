f6e8e2b37af92d7bf523c62fbf3370be4c4dbd3c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-24 09:34:45 +0100

    puesto todo en una sola clase para facilitar el código y creada la función plazo fijo, falta mejorarla

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,3 +1,5 @@
+
+from datetime import datetime
 class Cuenta_Bancaria:
     def __init__(self, ID, nombre_titular,fecha_apertura, numero_cuenta, saldo):
         self.ID=ID
@@ -34,9 +36,21 @@ class Cuenta_Bancaria:
         else:
             saldo_final = saldo - dinero_trasferir
             print(f"Su saldo final es {saldo_final} € y ha enviado a la cuenta de Rubén {dinero_trasferir} €")
+    def retirar_dinero_fijo():
+        datos =["ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300"]
+        saldo = datos.pop(4)
+        saldo = float(saldo)
+        dinero_retirar=float(input(f"Su saldo actual es de {saldo}\n¿Cuánto dinero desea retirar?\n"))
+        if saldo - dinero_retirar < 0:
+            print("\nOperación no válida, no tienes tanto capital")
+        else:
+            saldo_final = saldo - (dinero_retirar + saldo * 0.05)
+            print(f"Su saldo final es {saldo_final}")
+
+    print("Esta cuenta tiene un plazo fijo vigente desde ayer, por lo que habrá una comisión en sus operaciones")
     datos=["ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300€"]
     print(f"\nLos datos de su cuenta bancaria son los siguientes: {datos}\n")
-    print("\nQué desea hacer con su cuenta bancaria\n --> 1: Ingresar Dinero\n --> 2: Retirar Dinero\n --> 3: Trasferir Dinero\n")
+    print("\nQué desea hacer con su cuenta bancaria\n --> 1: Ingresar Dinero\n --> 2: Retirar Dinero\n --> 3: Trasferir Dinero\n --> 4: Cuenta Plazo fijo\n")
     eleccion=int(input())
     if eleccion == 1:
         ingresar_dinero()
@@ -44,8 +58,10 @@ class Cuenta_Bancaria:
         retirar_dinero()
     if eleccion == 3:
         trasferir_dinero()
+    if eleccion == 4:
+        retirar_dinero_fijo()
     else:
         exit()
-class Plazo_fijo(Cuenta_Bancaria):
-    pass
+    hora_actual=str(datetime.now().time())
+
 Cuenta_Bancaria
