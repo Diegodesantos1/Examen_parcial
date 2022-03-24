25ebac05cf94355e14760f743fff02c000ad4cf2
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-24 09:15:38 +0100

    reestructurado el código en una sola clase, ya que no veo necesario crear 2

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -7,20 +7,24 @@ class Cuenta_Bancaria:
         self.saldo=saldo
     def crear_cuenta():
         datos=["ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300€"]
-        print(datos)
-Cuenta_Bancaria.crear_cuenta()
-class Operaciones_cuenta(Cuenta_Bancaria):
     def ingresar_dinero():
         datos =["ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300"]
         print("¿Cuánto dinero quiere ingresar a su cuenta?")
         saldo = datos.pop(4)
-        print(f"Su saldo actual es de {saldo}")
+        print(f"Su saldo actual es de {saldo}€")
         saldo = float(saldo)
         dinero_añadir=float(input())
         dinero_total=saldo + dinero_añadir
         print(f"El saldo de su cuenta es {dinero_total} €")
     def retirar_dinero():
-        datos =["ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300€"]
+        datos =["ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300"]
+        saldo = datos.pop(4)
+        saldo = float(saldo)
+        dinero_retirar=float(input(f"Su saldo actual es de {saldo}\n¿Cuánto dinero desea retirar?"))
+        if saldo - dinero_retirar < 0:
+            print("\nOperación no válida, no tienes tanto capital")
+            Cuenta_Bancaria.retirar_dinero()
+
     def trasferir_dinero():
         datos =["ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300€"]
     print("Qué desea hacer con su cuenta bancaria\n --> 1: Ingresar Dinero\n --> 2: Retirar Dinero\n --> 3: Trasferir Dinero\n")
@@ -32,8 +36,4 @@ class Operaciones_cuenta(Cuenta_Bancaria):
     if eleccion == 3:
         trasferir_dinero()
 
-
-
-
-
-Operaciones_cuenta
+Cuenta_Bancaria.crear_cuenta()
