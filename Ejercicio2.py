04c41909ef8785d4a2bbdc77d15de7c2e8960290
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-24 08:44:55 +0100

    ejercicio 2 con las clases y herencias

---
+++ Ejercicio2.py
@@ -0,0 +1,16 @@
+class Animal:
+    pass
+
+class Mamífero(Animal):
+    pass
+
+class Ovíparo(Mamífero):
+    pass
+
+class Pollo(Ovíparo):
+    pass
+
+class Gato (Mamífero):
+    pass
+class Ornitorrinco(Ovíparo):
+    pass
