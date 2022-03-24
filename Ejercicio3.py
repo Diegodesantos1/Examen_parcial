0240467b2d61adadb3552447dce56a748e0a0fee
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-24 08:57:20 +0100

    planteada opciones de la cuenta

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -10,3 +10,15 @@ class Cuenta_Bancaria:
         return txt.format(self.ID, self.nombre_titular,self.fecha_apertura, self.numero_cuenta, self.saldo)
 A=Cuenta_Bancaria("ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300€")
 print(A.crear_cuenta())
+
+class Operaciones_cuenta(Cuenta_Bancaria):
+    eleccion = int("Qué desea hacer con su cuenta bancaria\n --> 1: Ingresar Dinero\n --> 2: Retirar Dinero\n --> 3: Trasferir Dinero\n")
+    if eleccion == 1:
+        def ingresar_dinero():
+            print("¿Cuánto dinero quiere ingresar a su cuenta?")
+
+
+
+
+
+Operaciones_cuenta
