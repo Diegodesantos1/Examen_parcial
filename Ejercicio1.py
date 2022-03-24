35f4f933f6293e5e1838f3ad656ab3ad2aa71e8e
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-24 08:42:01 +0100

    acabado el ejercicio con la clase, la función ocurrir libro y los prints

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,6 +1,13 @@
 
 class Libro:
-    def __init__(self, autor, año,nombre):
+    def __init__(self, autor, año,nombre, codigo):
         self.autor=autor
         self.año=año
         self.nombre=nombre
+        self.codigo=codigo
+    def ocurrir_Libro(self):
+        txt= "{0},{1},{2},{3}"
+        return txt.format(self.nombre, self.autor,self.año, self.codigo)
+
+A=Libro("Don Quijote","Cervantes", "1534", "12345678") ; B=Libro("La sombra del viento","Carlos Ruiz Zafón","2002", "536792") ; C=Libro("Harry Potter","J.K Rowling","1985", "6157368179")
+print(A.ocurrir_Libro()) ; print(B.ocurrir_Libro()) ; print(C.ocurrir_Libro())
