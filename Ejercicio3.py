4468062d6d2835173f6448d4574acd3e0c9bd61c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-24 08:53:55 +0100

    creada la cuenta bancaria y cambiado el nombre del la funcion de la clase

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -5,6 +5,8 @@ class Cuenta_Bancaria:
         self.fecha_apertura=fecha_apertura
         self.numero_cuenta=numero_cuenta
         self.saldo=saldo
-    def planteamiento(self):
+    def crear_cuenta(self):
         txt= "{0},{1},{2},{3},{4}"
         return txt.format(self.ID, self.nombre_titular,self.fecha_apertura, self.numero_cuenta, self.saldo)
+A=Cuenta_Bancaria("ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300€")
+print(A.crear_cuenta())
