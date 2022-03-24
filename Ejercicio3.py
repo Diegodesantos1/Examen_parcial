c6108ad4a421983d4d621896d34c51181941d4af
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-24 09:01:59 +0100

    puestos los datos en una lista, ya que así son más simples de trabajar

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -5,17 +5,17 @@ class Cuenta_Bancaria:
         self.fecha_apertura=fecha_apertura
         self.numero_cuenta=numero_cuenta
         self.saldo=saldo
-    def crear_cuenta(self):
-        txt= "{0},{1},{2},{3},{4}"
-        return txt.format(self.ID, self.nombre_titular,self.fecha_apertura, self.numero_cuenta, self.saldo)
-A=Cuenta_Bancaria("ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300€")
-print(A.crear_cuenta())
-
+    def crear_cuenta():
+        datos=["ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300€"]
+        print(datos)
+Cuenta_Bancaria.crear_cuenta()
 class Operaciones_cuenta(Cuenta_Bancaria):
-    eleccion = int("Qué desea hacer con su cuenta bancaria\n --> 1: Ingresar Dinero\n --> 2: Retirar Dinero\n --> 3: Trasferir Dinero\n")
+    print("Qué desea hacer con su cuenta bancaria\n --> 1: Ingresar Dinero\n --> 2: Retirar Dinero\n --> 3: Trasferir Dinero\n")
+    eleccion=int(input())
     if eleccion == 1:
         def ingresar_dinero():
             print("¿Cuánto dinero quiere ingresar a su cuenta?")
+
             dinero_añadir=float(input())
             dinero_total=float(saldo) + dinero_añadir
 
