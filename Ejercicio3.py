19965667f9bd36b50daaf08f1cb8e6aab47dbc03
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-24 09:50:04 +0100

    reusadas funciones y adaptadas al vip y reestructuración general del codigo

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -48,21 +48,57 @@ class Cuenta_Bancaria:
         else:
             saldo_final = saldo - (dinero_retirar + saldo * 0.05)
             print(f"Su saldo final es {saldo_final}")
+    def retirar_dinero_VIP():
+        datos =["ID1234","Diego de Santos", "10/05/2022", "ES12345678", "3000"]
+        saldo = datos.pop(4)
+        saldo = float(saldo)
+        dinero_retirar=float(input(f"Su saldo actual es de {saldo}\n¿Cuánto dinero desea retirar?\n"))
+        saldo_final = saldo - dinero_retirar
+        print(f"Su saldo final es {saldo_final}")
+    def trasferir_dinero():
+        datos =["ID1234","Diego de Santos", "10/05/2022", "ES12345678", "3000"]
+        saldo = datos.pop(4)
+        saldo = float(saldo)
+        dinero_trasferir=float(input(f"Su saldo actual es de {saldo}\n¿Cuánto dinero desea trasferir?\n"))
+        saldo_final = saldo - dinero_trasferir
+        print(f"Su saldo final es {saldo_final} € y ha enviado a la cuenta de Rubén {dinero_trasferir} €")
     datos=["ID1234","Diego de Santos", "10/05/2022", "ES12345678", "300€"]
     print(f"\nLos datos de su cuenta bancaria son los siguientes: {datos}\n")
     print("\nQué desea hacer con su cuenta bancaria\n --> 1: Cuenta corriente\n --> 2: Cuenta Plazo\n --> 3: Cuenta Vip")
     tipo_cuenta = int(input())
-    print("\nQué desea hacer con su cuenta bancaria\n --> 1: Ingresar Dinero\n --> 2: Retirar Dinero\n --> 3: Trasferir Dinero\n --> 4: Plazo fijo (Sólo válido para cuentas Plazo)\n")
-    eleccion=int(input())
-    if eleccion == 1 and tipo_cuenta == 1:
-        ingresar_dinero()
-    elif eleccion == 2 and tipo_cuenta == 1:
-        retirar_dinero()
-    elif eleccion == 3 and tipo_cuenta == 1:
-        trasferir_dinero()
-    if eleccion == 4 and tipo_cuenta == 4:
-        retirar_dinero_fijo()
-    else:
-        exit()
+    if tipo_cuenta == 1:
+        print("\nQué desea hacer con su cuenta bancaria\n --> 1: Ingresar Dinero\n --> 2: Retirar Dinero\n --> 3: Trasferir Dinero\n --> 4: Plazo fijo (Sólo válido para cuentas Plazo)\n")
+        eleccion=int(input())
+        if eleccion == 1:
+            ingresar_dinero()
+        elif eleccion == 2:
+            retirar_dinero()
+        elif eleccion == 3:
+            trasferir_dinero()
+        else:
+            exit()
+    elif tipo_cuenta == 2:
+        print("\nQué desea hacer con su cuenta Plazo Fijo\n --> 1: Ingresar Dinero\n --> 2: Retirar Dinero\n --> 3: Trasferir Dinero\n")
+        eleccion=int(input())
+        if eleccion == 1:
+            ingresar_dinero()
+        elif eleccion == 2:
+            retirar_dinero_fijo()
+        elif eleccion == 3:
+            trasferir_dinero()
+        else:
+            exit()
+    elif tipo_cuenta == 3:
+        print("\nQué desea hacer con su cuenta VIP\n --> 1: Ingresar Dinero\n --> 2: Retirar Dinero\n --> 3: Trasferir Dinero\n")
+        eleccion=int(input())
+        if eleccion == 1:
+            ingresar_dinero()
+        elif eleccion == 2:
+            retirar_dinero_VIP()
+        elif eleccion == 3:
+            trasferir_dinero_VIP()
+        else:
+            exit()
+
 
 Cuenta_Bancaria
